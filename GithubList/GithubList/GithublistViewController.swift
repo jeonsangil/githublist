b85f6f9c8c9cab92d8a2130cde5659c735916c3a
JEONSANGIL <sangil0526@gmai.com>
2017-12-10 22:20:45 +0900

    4 searbar  구현
    debouce 0.5 rx로 구현

--- GithubList/GithubList/GithublistViewController.swift
+++ GithubList/GithubList/GithublistViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 import SnapKit
+import RxCocoa
+import RxSwift
 
 final class GithublistViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate {
     
+    //UI 변수 관련
     var tableview : UITableView!
     var searchBar: UISearchBar!
     
+    //통신 데이타 관련
     let apiList: ApiList = ApiList()
+    //searbar 관련
+    var searchData : [ApiList.Response] = [ApiList.Response]() //검색데이타 저장
+    var inSearchMode = false //서치모드 플래그
+    //rx 관련
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +37,13 @@ final class GithublistViewController: UIViewController, UISearchBarDelegate, UIT
     }
     //뷰 UI 및 데이타 셋팅
     func setting(){
-        
+        //서치바 셋팅
         self.searchBar = UISearchBar()
         self.searchBar.placeholder = "Search"
         self.searchBar.delegate = self
         self.searchBar.returnKeyType = UIReturnKeyType.done
         self.view.addSubview(self.searchBar)
-        
+    
         self.searchBar.snp.makeConstraints { (make) -> Void in
             if #available(iOS 11.0, *) {
                 make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(0)
@@ -45,8 +54,19 @@ final class GithublistViewController: UIViewController, UISearchBarDelegate, UIT
             make.leading.equalTo(0)
             make.trailing.equalTo(0)
         }
+        //searchbar rx코딩 웹사이트 참조
+        /* https://pilgwon.github.io/blog/2017/09/26/RxSwift-By-Examples-1-The-Basics.html */
+        self.searchBar
+            .rx.text // RxCocoa의 Observable 속성
+            .orEmpty // 옵셔널이 아니도록 만듭니다.
+            .debounce(0.5, scheduler: MainScheduler.instance) // 0.5초 기다립니다.
+            .distinctUntilChanged() // 새로운 값이 이전의 값과 같은지 확인합니다.
+            .subscribe(onNext: { [unowned self] query in // 여기서 새로운 값에 대한 구독을 합니다.
+                self.tableview.reloadData()
+            })
+            .disposed(by: disposeBag)
         
-        
+        //테이블뷰 셋팅
         self.tableview = UITableView()
         self.tableview.register(ListTableViewCell.self, forCellReuseIdentifier: "Cell")
         self.tableview.separatorStyle = .none
@@ -64,7 +84,6 @@ final class GithublistViewController: UIViewController, UISearchBarDelegate, UIT
         self.apiRequest(listId: "since")
     }
 }
-
 //network
 extension GithublistViewController  {
     func apiRequest(listId : String){
@@ -73,19 +92,48 @@ extension GithublistViewController  {
         }
     }
 }
-
 //tableview
 extension GithublistViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if inSearchMode {
+            return searchData.count
+        }
         return self.apiList.response.count
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ListTableViewCell
-        //Cell 데이타 삽입
-        cell.setData(item: self.apiList.response[indexPath.row])
+        
+        //검색 데이타 삽입
+        if inSearchMode {
+            cell.setData(item: self.searchData[indexPath.row])
+        }else{ //Cell 데이타 삽입
+            cell.setData(item: self.apiList.response[indexPath.row])
+        }
         return cell
+    }
+}
+//saerchBar 구현
+extension GithublistViewController  {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text == nil || searchBar.text == "" {
+            
+            inSearchMode = false
+            
+            view.endEditing(true)
+            
+        } else {
+            
+            inSearchMode = true
+            
+            self.searchData = self.apiList.response.filter({  $0.name == searchBar.text})
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
     }
 }
 
